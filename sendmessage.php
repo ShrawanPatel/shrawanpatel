2733e4fe8c8fe2330ccf4d398abd5334d0b1c1b5
Shrawan Patel <shrawan.patel@doctors.net.uk>
2016-03-30 07:40:10 +0100

    initial upload of base code

---
+++ sendmessage.php
@@ -0,0 +1,31 @@
+<?
+    /* This script emails whatever is posted to it and
+    * requires the form to have a hidden input named "redirect"
+    */
+
+   // The redirect url is required and prevents non-form-posted processing.
+   if (isset($_POST['redirect']))
+   {
+      // Set up message
+      $send_to = $_POST['send_to'];
+      $send_from = $_POST['send_from'];
+      $subject  = $_POST['subject'];
+      $redirect  = $_POST['redirect'];
+
+      // Set up the header
+      $header  = "From: " . $send_from . "\r\n";
+      $header .= "X-Mailer: PHP/" . phpversion();
+
+      // Build the message
+      foreach ($_POST as $key=>$value)
+      {
+         $message .= $key . ": " . $value . "\r\n";
+      }
+
+      // Send the email
+      // The '@' surpresses errors.
+      @mail($send_to, $subject, $message, $header);
+
+      header("Location: " . $redirect);
+   }
+?>
